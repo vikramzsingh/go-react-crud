3a1bdf24c2864c6448906601fec6d4b70424845a
vikramzsingh <vikramsinghmoal@gmail.com>
2021-03-03 18:01:01 +0530

    Vakidation added form

--- fetch-app/src/components/pages/CreateUser.js
+++ fetch-app/src/components/pages/CreateUser.js
@@ -59,34 +59,48 @@ class CreateUser extends Component {
         return (
             <div className="container">
                 <h1 className="text-center text-primary">Create User</h1>
-                <form onSubmit={this.submitHandler}> 
+                <form onSubmit={this.submitHandler} className="was-validated"> 
                     <div className="mb-3">
                         <label htmlFor="fName" className="form-label">First name</label>
-                        <input type="text" name="fname" value={fname} className="form-control" id="fname" onChange={this.changeHandler} />
+                        <input type="text" name="fname" value={fname} required className="form-control" id="fname" onChange={this.changeHandler} />
+                        <div class="valid-feedback">looks good</div>
+                        <div class="invalid-feedback">Please fill out this field</div>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="lName" className="form-label">Last name</label>
-                        <input type="text" name="lname" value={lname} className="form-control" id="age" onChange={this.changeHandler}/>
+                        <input type="text" name="lname" value={lname} required className="form-control" id="age" onChange={this.changeHandler}/>
+                        <div class="valid-feedback">looks good</div>
+                        <div class="invalid-feedback">Please fill out this field</div>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="age" className="form-label">Age</label>
-                        <input type="number" name="age" value={age} className="form-control" id="age" onChange={this.changeHandler}/>
+                        <input type="number" name="age" value={age} required className="form-control" id="age" onChange={this.changeHandler}/>
+                        <div class="valid-feedback">looks good</div>
+                        <div class="invalid-feedback">Please fill out this field</div>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="MobileNo" className="form-label">Phone number</label>
-                        <input type="text" name="mobileno" value={mobileno} className="form-control" id="mobileno" onChange={this.changeHandler}/>
+                        <input type="text" name="mobileno" value={mobileno} required className="form-control" id="mobileno" onChange={this.changeHandler}/>
+                        <div class="valid-feedback">looks good</div>
+                        <div class="invalid-feedback">Please fill out this field</div>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="DOB" className="form-label">Date of birth</label>
-                        <input type="text" name="dob" value={dob} className="form-control" id="dob" onChange={this.changeHandler}/>
+                        <input type="text" name="dob" value={dob} required className="form-control" id="dob" onChange={this.changeHandler}/>
+                        <div class="valid-feedback">looks good</div>
+                        <div class="invalid-feedback">Please fill out this field</div>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="Email" className="form-label">Email address</label>
-                        <input type="email" name="email" value={email} className="form-control" id="email" onChange={this.changeHandler}/>
+                        <input type="email" name="email" value={email} required className="form-control" id="email" onChange={this.changeHandler}/>
+                        <div class="valid-feedback">looks good</div>
+                        <div class="invalid-feedback">Please fill out this field</div>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="Address" className="form-label">Address</label>
-                        <input type="text" name="address" value={address} className="form-control" id="address" onChange={this.changeHandler}/>
+                        <input type="text" name="address" value={address} required className="form-control" id="address" onChange={this.changeHandler}/>
+                        <div class="valid-feedback">looks good</div>
+                        <div class="invalid-feedback">Please fill out this field</div>
                     </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
--- fetch-app/src/components/pages/Index.js
+++ fetch-app/src/components/pages/Index.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
 
-export default function Index() {
-    return (
-        <div className="container">
-            <h1>Welcome</h1>
-        </div>
-    )
+function Index() {
+        return (
+            <div className="container text-center text-primary">
+                <h1>Welcome</h1>
+                <NavLink className="nav-link" exact to="/">Home</NavLink>
+                <NavLink className="nav-link" exact to="/createuser">Create User</NavLink>
+                <NavLink className="nav-link" exact to="/getuser">Get User</NavLink>
+                <NavLink className="nav-link" exact to="/updateuser">Update User</NavLink>
+                <NavLink className="nav-link" exact to="/deleteuser">Delete User</NavLink>
+            </div>
+        )
 }
+
+export default Index
